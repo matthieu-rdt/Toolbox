0be88c598f93af1735ba4ea4680988fe21c3abe1
Matthieu <67316829+matthieu-rdt@users.noreply.github.com>
2022-05-12 16:52:26 +0200

    Update echo_text.sh

--- echo_text.sh
+++ echo_text.sh
@@ -1,11 +1,10 @@
-echo_text=(
+declare -a echo_text=(
 "line 1"
 "line 2"
 "line 3"
 )
 
-array_length=${#echo_text[@]}
-for (( i=0; i<=array_length; i++ ))
+for i in "${echo_test[@]}"
 do
-	echo "${echo_test[$i]} | sudo tee -a /file/path
+	echo $i
 done
