9b4406203c3f80aeb8178fd3b1f42802ced84b7d
Matthieu <matt.doff@gmail.com>
2022-06-14 10:28:18 +0200

    First up

---
+++ ConfirmChoice.sh
@@ -0,0 +1,11 @@
+# Function from Manu
+function ConfirmChoice ()
+{
+        ConfYorN="";
+                while [ "${ConfYorN}" != "y" -a "${ConfYorN}" != "Y" -a "${ConfYorN}" != "n" -a "${ConfYorN}" != "N" ]
+                do
+                        echo -n $1 "(y/n) : "
+                        read ConfYorN
+                done
+        [ "${ConfYorN}" == "y" -o "${ConfYorN}" == "Y" ] && return 0 || return 1
+}
