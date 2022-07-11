cc5a096b624b7de057f21656aec12385c44aa394
Matthieu <matt.doff@gmail.com>
2022-07-11 10:09:29 +0200

    Let the user choose

--- SSH.sh
+++ SSH.sh
@@ -24,37 +24,40 @@ function ConfirmChoice()
 #	Start	    #
 #-------------------#
 
-if [ ! -d .ssh ] ; then
+if	[ ! -d .ssh ] ; then
 	mkdir ~/.ssh
 fi
 
 #	Using an existing passphrase
 ConfirmChoice "Do you have a passphrase to use" && read -s passwd
 
-#	Creating a secure passphrase if passwd is empty
-if [ -z "$passwd" ] ; then
-	echo "Choose if you prefer a 128 bits OR a 256 bits passphrase" && sleep 2
+#	Creating a secure passphrase if needed
+ConfirmChoice "Generate a passphrase with 128 bits of entropy ?" && passwd=`dd if=/dev/urandom bs=16 count=1 2>/dev/null | base64 | sed 's/=//g'`
+echo $passwd > $HOME/passph.txt
 
-	ConfirmChoice "Generate a passphrase with 128 bits of entropy ?" && passwd=`dd if=/dev/urandom bs=16 count=1 2>/dev/null | base64 | sed 's/=//g'`
-	echo $passwd > $HOME/passph.txt
+ConfirmChoice "Generate a passphrase with 256 bits of entropy ?" && passwd=`dd if=/dev/urandom bs=32 count=1 2>/dev/null | sha256sum -b | sed 's/ .*//'`
+echo $passwd > $HOME/passph.txt
 
-	ConfirmChoice "Generate a passphrase with 256 bits of entropy ?" && passwd=`dd if=/dev/urandom bs=32 count=1 2>/dev/null | sha256sum -b | sed 's/ .*//'`
-	echo $passwd > $HOME/passph.txt
-
-	chmod 400 $HOME/passph.txt && echo "Your passphrase is located in $HOME/passph.txt"
-fi
+chmod 400 $HOME/passph.txt && echo "Your passphrase is located in $HOME/passph.txt"
 
 ##	Generation of a public/private key pair
 #	Encryption algorithms
 
 read -p 'give your ssh key a name : ' keyname
 
-echo "The script suggests one of the encryption algorithms below"
+echo "One of the encryption algorithms is recommended"
 echo "RSA | ED25519" && sleep 2
 
-ConfirmChoice "RSA , Proven and recommended with a key size of 4096 bits. Compatible everywhere" && ssh-keygen -a 100 -b 4096 -N "$passwd" -f ~/.ssh/$keyname # by default so no need for "-t"
+if 	[ -f "$passwd" ] ; then
+	ConfirmChoice "RSA , Proven and recommended with a key size of 4096 bits. Compatible everywhere" && ssh-keygen -a 100 -b 4096 -N "$passwd" -f ~/.ssh/$keyname # by default so no need for "-t"
 
-ConfirmChoice "ED25519 , The latest and greatest in terms of safety and performance" && ssh-keygen -a 100 -f ~/.ssh/$keyname -N "$passwd" -t ed25519
+	ConfirmChoice "ED25519 , The latest and greatest in terms of safety and performance" && ssh-keygen -a 100 -f ~/.ssh/$keyname -N "$passwd" -t ed25519
+
+else
+	ConfirmChoice "RSA , Proven and recommended with a key size of 4096 bits. Compatible everywhere" && ssh-keygen -a 100 -b 4096 -N "$passwd" -f ~/.ssh/$keyname # by default so no need for "-t"
+
+	ConfirmChoice "ED25519 , The latest and greatest in terms of safety and performance" && ssh-keygen -a 100 -f ~/.ssh/$keyname -N "$passwd" -t ed25519
+fi
 
 read -p 'Type the address of the remote server in this form: (example: 192.168.0.20) ' address 
 
