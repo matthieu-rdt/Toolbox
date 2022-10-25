8c9b31fddb2c0d4f9d6be19df0abd76fe24ff24b
Matthieu <matt.doff@gmail.com>
2022-10-25 12:45:29 +0200

    Added some lines in functions

--- Linux/SSH.sh
+++ Linux/SSH.sh
@@ -20,23 +20,53 @@ ConfirmChoice()
 	[ "${ConfYorN}" == "y" -o "${ConfYorN}" == "Y" ] && return 0 || return 1
 }
 
+use_passphrase ()
+{
+	#	Using an existing passphrase
+	ConfirmChoice "Do you have a passphrase to use" && read -s passwd
+
+	#	Creating a secure passphrase if needed
+	ConfirmChoice "Generate a passphrase with 128 bits of entropy ?" && passwd=`dd if=/dev/urandom bs=16 count=1 2>/dev/null | base64 | sed 's/=//g'` \
+	echo $passwd > $HOME/passph128.txt
+
+	ConfirmChoice "Generate a passphrase with 256 bits of entropy ?" && passwd=`dd if=/dev/urandom bs=32 count=1 2>/dev/null | sha256sum -b | sed 's/ .*//'` \
+	echo $passwd > $HOME/passph256.txt
+}
+
+generate_ssh_keys ()
+{
+	echo "One of the encryption algorithms is recommended"
+	echo "RSA | ED25519" && sleep 2
+
+	while [ -z $keyname ] ; do
+		echo "Note : If the key name is already existing, you can choose to overwrite it or not"
+		echo "Default name is either <id_rsa> or <id_ed25519>, a name is required"
+		read -p 'Give your ssh key a name : ' keyname
+	done
+
+	if 	[ -f "$passwd" ] ; then
+		# by default so no need for "-t"
+		ConfirmChoice "RSA , Proven and recommended with a key size of 4096 bits. Compatible everywhere" && ssh-keygen -a 100 -b 4096 -N "$passwd" -f $HOME/.ssh/$keyname
+
+		ConfirmChoice "ED25519 , The latest and greatest in terms of safety and performance" && ssh-keygen -a 100 -f $HOME/.ssh/$keyname -N "$passwd" -t ed25519
+
+	else
+	#	by default so no need for "-t"
+		ConfirmChoice "RSA , Proven and recommended with a key size of 4096 bits. Compatible everywhere" && ssh-keygen -a 100 -b 4096 -f $HOME/.ssh/$keyname
+
+		ConfirmChoice "ED25519 , The latest and greatest in terms of safety and performance" && ssh-keygen -a 100 -f $HOME/.ssh/$keyname -t ed25519
+	fi
+}
+
 #-------------------#
 #	Start	    #
 #-------------------#
 
 if	[ ! -d .ssh ] ; then
-	mkdir ~/.ssh
+	mkdir $HOME/.ssh
 fi
 
-#	Using an existing passphrase
-ConfirmChoice "Do you have a passphrase to use" && read -s passwd
-
-#	Creating a secure passphrase if needed
-ConfirmChoice "Generate a passphrase with 128 bits of entropy ?" && passwd=`dd if=/dev/urandom bs=16 count=1 2>/dev/null | base64 | sed 's/=//g'` \
-echo $passwd > $HOME/passph.txt
-
-ConfirmChoice "Generate a passphrase with 256 bits of entropy ?" && passwd=`dd if=/dev/urandom bs=32 count=1 2>/dev/null | sha256sum -b | sed 's/ .*//'` \
-echo $passwd > $HOME/passph.txt
+use_passphrase
 
 if	[ -f "$passwd" ] ; then
 	chmod 400 $HOME/passph.txt && echo "Your passphrase is located in $HOME/passph.txt"
@@ -45,44 +75,28 @@ fi
 ##	Generation of a public/private key pair
 #	Encryption algorithms
 
-echo "Default name is either <id_rsa> or <id_ed25519>, a name is required"
-read -p 'Give your ssh key a name : ' keyname
-
-echo "One of the encryption algorithms is recommended"
-echo "RSA | ED25519" && sleep 2
-
-if 	[ -f "$passwd" ] ; then
-	ConfirmChoice "RSA , Proven and recommended with a key size of 4096 bits. Compatible everywhere" && ssh-keygen -a 100 -b 4096 -N "$passwd" -f ~/.ssh/$keyname # by default so no need for "-t"
-
-	ConfirmChoice "ED25519 , The latest and greatest in terms of safety and performance" && ssh-keygen -a 100 -f ~/.ssh/$keyname -N "$passwd" -t ed25519
-
-else
-#	by default so no need for "-t"
-	ConfirmChoice "RSA , Proven and recommended with a key size of 4096 bits. Compatible everywhere" && ssh-keygen -a 100 -b 4096 -f ~/.ssh/$keyname
-
-	ConfirmChoice "ED25519 , The latest and greatest in terms of safety and performance" && ssh-keygen -a 100 -f ~/.ssh/$keyname -t ed25519
-fi
+generate_ssh_keys
 
 read -p 'Type the address of the remote server in this form: (example: 192.168.1.2) ' address
 
 read -p 'Type in the remote server login - this is the server user : ' login
 
 #	Checking the data entered by the client
-echo "your login and IP address are: $login@$address"
+echo "your login and IP address are : $login@$address"
 
 #	Send the public key to the desired server
-ssh-copy-id -i ~/.ssh/$keyname.pub $login@$address
+ssh-copy-id -i $HOME/.ssh/$keyname.pub $login@$address
 
 #	Make SSH connection easier
-echo "Creating ~/.ssh/config"
-echo "Usage ssh <customised hostname>"
+echo "Creating $HOME/.ssh/config"
+echo "Usage 'ssh <customised hostname>'"
 
 read -p 'Name your machine : ' hostname
 
-tee -a ~/.ssh/config << END
+tee -a $HOME/.ssh/config << END
 Host $hostname
 	HostName $address
 	User $login
-	IdentityFile ~/.ssh/$keyname
+	IdentityFile $HOME/.ssh/$keyname
 	IdentitiesOnly yes
 END
