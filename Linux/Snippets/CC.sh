073d49b3829c49f4601ba666cd1ffbe882a5833e
Matthieu <matt.doff@gmail.com>
2022-12-20 12:07:41 +0100

    Useless

--- Linux/Snippets/CC.sh
+++
@@ -1,7 +0,0 @@
-wget https://raw.githubusercontent.com/matthieu-rdt/Toolbox/master/Linux/ConfirmChoice.sh
-
-source $HOME/ConfirmChoice.sh
-
-ConfirmChoice "echo ok" && echo ok
-
-rm $HOME/ConfirmChoice.sh
